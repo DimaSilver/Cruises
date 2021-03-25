c838698d29c5112464570e01d6046d644490b450
DimaSilver <dimka7600@ukr.net>
2021-03-25 23:15:46 +0300

    Застилизовал сэкшены header и first-screen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-    ('body').slick({});
-})
+    $('select').styler();
+
+    $('.menu__btn').on("click", function () {
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+    $('.btn__link').on("click", function () {
+        $('.btn__link').toggleClass('btn__link--active');
+    });
+});
