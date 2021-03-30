d032e47fb28a5dec2e44e1de0b67b5c696f2e4ba
DimaSilver <dimka7600@ukr.net>
2021-03-30 07:24:09 +0300

    Popup(HTML + стилизация)

--- js/main.js
+++ js/main.js
@@ -7,4 +7,18 @@ $(function () {
     $('.btn__link').on("click", function () {
         $('.btn__link').toggleClass('btn__link--active');
     });
+    $('.catalog__item-textarea__prices-new').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#card-form',
+        callbacks: {
+            beforeOpen: function() {
+                if($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#card-form';
+                }
+            }
+        }
+    });
 });
